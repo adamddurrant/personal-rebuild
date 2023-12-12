6e9f9fb3f2e2725183156919046038222af37d9f
Adam Durrant <adam@Adams-MacBook-Pro.local>
2023-12-12 21:36:48 +0000

    Strip blog post and start fetching data from dato

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,8 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { getAllPublished, getSingleBlogPostBySlug } from "../../lib/notion";
+import { getPosts } from "../../lib/api";
+import { request } from "../../lib/datocms";
 import util from "../../styles/util.module.css";
 import style from "./markdownStyles.module.css";
 import gfm from "remark-gfm";
@@ -16,103 +17,70 @@ const CodeBlock = ({ language, codestring }) => {
     </SyntaxHighlighter>
   );
 };
-const Post = ({ post }) => {
+export default function Post (props) {
+  const { data } = props;
   return (
     <>
-      <Head>
-        <title>{post.metadata.title} | ADurrant</title>
-        <meta name='description' content={post.metadata.description} />
-        <meta property='og:title' content={post.metadata.title} />
-        <meta property='og:description' content={post.metadata.description} />
-        <meta name='twitter:title' content={post.metadata.title} />
-        <meta name='twitter:description' content={post.metadata.description} />
-      </Head>
-      <article className={util.page} id='aboutPage'>
-        <div className={util.pageColumn}>
-          <h1 style={{ marginTop: "20px" }} className={util.blogHeader}>
-            {post.metadata.title}
-          </h1>
-          <div
-            className={util.tags + " " + util.flexColumn + " " + util.postMeta}>
-            <div
-              className={util.flexMeta}
-              style={{ justifyContent: "center", alignItems: "center" }}>
-              <span className={util.postDate}>{post.metadata.date}</span>
-              <p className={post.metadata.tags[0].color + "Tag tag"}>
-                {post.metadata.tags[0].name}
-              </p>
-            </div>
 
-            <div className={util.flexRow} style={{ marginTop: "30px" }}>
-              <div style={{ marginRight: "10px" }}>
-                <Image
-                  priority
-                  width={40}
-                  height={40}
-                  src='/me/adam-durrant.jpg'
-                  alt='Adam Durrant'
-                  style={{ borderRadius: "100px" }}
-                />
-              </div>
-              <div className={util.read}>
-                <p style={{ margin: "0px", lineHeight: "0.9" }}>Adam Durrant</p>
-                <a
-                  className={util.tweetLink}
-                  style={{ fontSize: "12px" }}
-                  href='https://twitter.com/AdamDDurrant'>
-                  @adamddurrant
-                </a>
-              </div>
-            </div>
-          </div>
-          <ReactMarkdown
-            remarkPlugins={[gfm]}
-            className={style.reactMarkDown}
-            components={{
-              code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || "");
-                return !inline && match ? (
-                  <CodeBlock
-                    codestring={String(children).replace(/\n$/, "")}
-                    language={match[1]}
-                  />
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}>
-            {post.markdown}
-          </ReactMarkdown>
-        </div>
-        <Link scroll={false} href='/blog'>
-          <div className={util.buttonPadding}>
-            <a className={util.backButton}> ← &nbsp; Back to blog</a>
-          </div>
-        </Link>
-      </article>
     </>
   );
 };
 
-export const getStaticProps = async ({ params }) => {
-  const post = await getSingleBlogPostBySlug(params.slug);
-  return {
-    props: {
-      post,
-    },
-    revalidate: 5,
-  };
-};
+// export async function getStaticProps({ params, preview = false }) {
+//   const data = await getPostAndMorePosts(params.slug, preview);
+//   const content = await markdownToHtml(data?.post?.content || "");
 
-export const getStaticPaths = async () => {
-  const posts = await getAllPublished();
-  const paths = posts.map(({ slug }) => ({ params: { slug } }));
+//   return {
+//     props: {
+//       preview,
+//       post: {
+//         ...data?.post,
+//         content,
+//       },
+//       morePosts: data?.morePosts ?? [],
+//     },
+//   };
+// }
+
+
+const PATHS_QUERY = ` 
+query Paths {
+  allPosts {
+    slug
+  }
+}
+`;
+
+export async function getStaticPaths() {
+  const slugQuery = await request({
+    query: PATHS_QUERY
+  });
+  let paths = [];
+  slugQuery.allPosts.map((p) => paths.push(`/blog/${p.slug}`));
+
   return {
     paths,
     fallback: false,
   };
-};
+}
 
-export default Post;
+
+const POST_QUERY = ` 
+query Posts {
+  allPosts {
+    title
+    publishDate
+    slug
+  }
+}
+`;
+
+// Props functon from dato
+export async function getStaticProps() {
+  const data = await request({
+    query: POST_QUERY
+  });
+  return {
+    props: { data }
+  };
+}
