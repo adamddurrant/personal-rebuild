ab2efc373c814923d71bb57daf00ece0a6ddb574
Adam Durrant <adam@Adams-MacBook-Pro.local>
2023-12-14 19:20:45 +0000

    More blog post styles

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,12 +1,12 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import styles from "../../pages/blog/index.module.css"
 import { request } from "../../lib/datocms";
 import { Image, renderRule, StructuredText } from "react-datocms"
 import util from "../../styles/util.module.css";
 import { isCode } from 'datocms-structured-text-utils';
 import Head from "next/head";
 import Link from "next/link";
-import { Image as NextImage } from "next/image";
 import authorImage from "../../public/me/adam-durrant.jpg";
 
 const CodeBlock = ({ language, codestring }) => {
@@ -29,41 +29,41 @@ export default function Blogpost(props) {
         <meta name='twitter:title' content={post.metadata.title} />
         <meta name='twitter:description' content={post.metadata.description} /> */}
       </Head>
-      <article className={util.page} id='aboutPage'>
+      <article className={util.page}>
+
         <div className={util.pageColumn}>
-          <Image data={postData.featuredImage.responsiveImage} />
-          <h1 style={{ marginTop: "20px" }} className={util.blogHeader}>
-            {postData.title}
-          </h1>
-          <div
-            className={util.tags + " " + util.flexColumn + " " + util.postMeta}>
-            <div
-              className={util.flexMeta}
-              style={{ justifyContent: "center", alignItems: "center" }}>
 
-              {/* <span className={util.postDate}>{post.metadata.date}</span> */}
+          <section className={styles.headerGroup}>
 
-              {/* <p className={post.metadata.tags[0].color + "Tag tag"}>
-                {post.metadata.tags[0].name}
-              </p> */}
-
+            <div className={styles.header}>
+              <h1 style={{ marginTop: "20px", textAlign: "left" }} className={util.blogHeader}>
+                {postData.title}
+              </h1>
+              <p className={styles.excerpt}>{postData.excerpt}</p>
             </div>
 
-            <div className={util.flexRow} style={{ marginTop: "30px" }}>
-              <div style={{ marginRight: "10px" }}>
-              <Image data={authorImage} style={{ borderRadius: "100px", width: "40px", height: "40px" }} />
-              </div>
-              <div className={util.read}>
-                <p style={{ margin: "0px", lineHeight: "0.9" }}>Adam Durrant</p>
-                <a
-                  className={util.tweetLink}
-                  style={{ fontSize: "12px" }}
-                  href='https://twitter.com/AdamDDurrant'>
-                  @adamddurrant
-                </a>
+            <div className={util.flexColumn + " " + styles.metaData}>
+              <div className={util.flexRow} style={{ marginTop: "30px" }}>
+                <div style={{ marginRight: "10px" }}>
+                  <Image data={authorImage} style={{ borderRadius: "100px", width: "40px", height: "40px" }} />
+                </div>
+                <div className={util.read}>
+                  <p style={{ margin: "0px", lineHeight: "0.9" }}>Adam Durrant</p>
+                  <a
+                    className={util.tweetLink}
+                    style={{ fontSize: "12px" }}
+                    href='https://twitter.com/AdamDDurrant'>
+                    @adamddurrant
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+
+            <div className={styles.featuredImage}>
+              <Image data={postData.featuredImage.responsiveImage} style={{ borderRadius: "6px" }} />
+            </div>
+
+          </section>
 
           <section className={util.body}>
 
@@ -79,9 +79,11 @@ export default function Blogpost(props) {
               }
               customNodeRules={[
                 renderRule(isCode, ({ node, key }) => (
-                  <CodeBlock key={key} codestring={node.code} language={node.language}>
-                    {node.code}
-                  </CodeBlock>
+                  <div className={styles.codeBlock}>
+                    <CodeBlock key={key} codestring={node.code} language={node.language}>
+                      {node.code}
+                    </CodeBlock>
+                  </div>
                 ))
               ]}
             />
@@ -89,11 +91,13 @@ export default function Blogpost(props) {
           </section>
 
         </div>
+
         <Link scroll={false} href='/blog'>
           <div className={util.buttonPadding}>
             <a className={util.backButton}> ← &nbsp; Back to blog</a>
           </div>
         </Link>
+
       </article>
     </>
   );
@@ -175,7 +179,16 @@ query singlePost($slug: String) {
         }
       }
     }
+    excerpt
     publishDate
+    seo {
+      description
+      image {
+        url
+      }
+      title
+      twitterCard
+    }
   }
 }
 `;
